ba7608662da4dde63a29743b26be7753e525a96b
Uncle Sam <uncle___sam@outlook.com>
2020-05-21 12:19:37 +0300

    server ip as parameter

--- Ethernet_linux/test_ETH.sh
+++ Ethernet_linux/test_ETH.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-iperf3 -c 192.168.1.1 -i 1 -t 30
-iperf3 -c 192.168.1.1 -i 1 -t 20 -R
-iperf3 -c 192.168.1.1 -i 1 -t 20 -w 32K -P 4
-iperf3 -c 192.168.1.1 -u -i 1 -b0
-iperf3 -c 192.168.1.1 -i.5 -O 2
-iperf3 -Z -c 192.168.1.1
-iperf3 -c 192.168.1.1
-iperf3 -c 192.168.1.1 -J
-iperf3 -c 192.168.1.1 -i 1 -t 30 -u -b0
-iperf3 -c 192.168.1.1 -i 1 -t 20 -R -u -b0
-iperf3 -c 192.168.1.1 -i 1 -t 20 -w 32K -P 4 -u -b0
-iperf3 -c 192.168.1.1 -i 1 -b0
-iperf3 -c 192.168.1.1 -i.5 -O 2 -u -b0
-iperf3 -Z -c 192.168.1.1 -u -b0
-iperf3 -c 192.168.1.1 -u -b0
-iperf3 -c 192.168.1.1 -T s1 -u -b0
-iperf3 -c 192.168.1.1 -J -u -b0
+echo server $1
+iperf3 -c $1 -i 1 -t 30
+iperf3 -c $1 -i 1 -t 20 -R
+iperf3 -c $1 -i 1 -t 20 -w 32K -P 4
+iperf3 -c $1 -u -i 1 -b0
+iperf3 -c $1 -i.5 -O 2
+iperf3 -Z -c $1
+iperf3 -c $1
+iperf3 -c $1 -J
+iperf3 -c $1 -i 1 -t 30 -u -b0
+iperf3 -c $1 -i 1 -t 20 -R -u -b0
+iperf3 -c $1 -i 1 -t 20 -w 32K -P 4 -u -b0
+iperf3 -c $1 -i 1 -b0
+iperf3 -c $1 -i.5 -O 2 -u -b0
+iperf3 -Z -c $1 -u -b0
+iperf3 -c $1 -u -b0
+iperf3 -c $1 -T s1 -u -b0
+iperf3 -c $1 -J -u -b0
